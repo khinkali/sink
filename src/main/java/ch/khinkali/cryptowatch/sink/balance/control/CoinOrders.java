e8908878b3669886691c87cc7fc741bb0513ed9c
Robert Brem <robert.brem@isceco.admin.ch>
2018-02-22 15:13:05 +0100

    [BUG][1] added metrics for prometheus

--- src/main/java/ch/khinkali/cryptowatch/sink/balance/control/CoinOrders.java
+++ src/main/java/ch/khinkali/cryptowatch/sink/balance/control/CoinOrders.java
@@ -1,7 +1,9 @@
 package ch.khinkali.cryptowatch.sink.balance.control;
 
 import ch.khinkali.cryptowatch.sink.events.entity.OrderPlaced;
+import io.prometheus.client.Counter;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.ConcurrencyManagement;
 import javax.ejb.ConcurrencyManagementType;
 import javax.ejb.Singleton;
@@ -17,11 +19,21 @@ public class CoinOrders {
 
     private Map<String, OrderPlaced> orders = new ConcurrentHashMap<>();
 
+    private Counter createdOrders;
+
+    @PostConstruct
+    public void initMetrics() {
+        createdOrders = Counter
+                .build("total_orders_created", "Total orders created")
+                .register();
+    }
+
     public OrderPlaced get(final String orderId) {
         return orders.get(orderId);
     }
 
     public void apply(@Observes OrderPlaced event) {
+        createdOrders.inc();
         orders.putIfAbsent(event.getOrderId(), new OrderPlaced(event.getOrderId(), event.getCoinSymbol(), event.getAmount(), event.getUserId()));
     }
 
--- src/main/java/ch/khinkali/cryptowatch/sink/user/control/Users.java
+++ src/main/java/ch/khinkali/cryptowatch/sink/user/control/Users.java
@@ -4,8 +4,10 @@ import ch.khinkali.cryptowatch.sink.balance.entity.Coin;
 import ch.khinkali.cryptowatch.sink.events.entity.OrderPlaced;
 import ch.khinkali.cryptowatch.sink.user.entity.User;
 import ch.khinkali.cryptowatch.user.events.entity.UserCreated;
+import io.prometheus.client.Counter;
 import lombok.Getter;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.ConcurrencyManagement;
 import javax.ejb.ConcurrencyManagementType;
 import javax.ejb.Singleton;
@@ -27,8 +29,17 @@ public class Users {
     @Getter
     private final Map<String, User> users = new ConcurrentHashMap<>();
 
+    private Counter createdUsers;
+
+    @PostConstruct
+    public void initMetrics() {
+        createdUsers = Counter
+                .build("total_users_created", "Total user created")
+                .register();
+    }
+
     public void apply(@Observes UserCreated event) {
-        logger.info("userEvent: " + event);
+        createdUsers.inc();
         users.put(event.getUserId(), new User(event.getUserId(), event.getUsername()));
     }
 
